7f3609737c8d0e0708b47f9b755725391857cd39
zayn-tech-info <zayntech.info@gmail.com>
2025-11-09 00:52:37 +0100

    Update personal introduction in paragraphs and improve formatting in constants file

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,65 +1,65 @@
 import {
-	SiHtml5,
-	SiCss3,
-	SiJavascript,
-	SiReact,
-	SiTailwindcss,
-	SiBootstrap,
-	SiNodedotjs,
-	SiExpress,
-	SiMongodb,
-  } from "react-icons/si";
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiReact,
+  SiTailwindcss,
+  SiBootstrap,
+  SiNodedotjs,
+  SiExpress,
+  SiMongodb,
+} from "react-icons/si";
 
 export const paragraphs = [
-	"Hey, I'm Yakubu Abdulbasit, a self-taught developer passionate about turning ideas into clean, responsive, and user-friendly web experiences. I love building things that not only work but also feel great to use.",
-	"I've been working with JavaScript, React.js,Tailwind CSS, Node/Express and Mongoose, constantly sharpening my skills through real-world projects, collaborative builds, and daily practice. Whether it's crafting small UI components or developing full web applications, I enjoy every step of the creative process.",
-	"I'm currently open to opportunities where I can grow with a team, contribute to exciting products, and keep learning new tools and technologies. When I'm not coding, you'll find me exploring tech trends, helping friends debug, or sketching ideas for my next build."
-  ];
+  "Hey there, I'm Yakubu Abdulbasit, an experienced web developer with years of developing web applications. I am proficient in architecting projects from the ground up as a full stack developer with hands-on experience in front-end and back-end technologies including HTML, CSS, JavaScript, React, Node/Express, and MongoDB. My strengths include mentoring junior developers and interns, and communicating with internal and external teams.",
+  "By nature I am analytical, quick to learn, and enjoy problem-solving. I approach projects with attention to detail and a focus on maintainable, scalable solutions.",
+  "Outside of work, I enjoy spending time in nature and exploring new ideas beyond my field. I'm naturally curious and love learning about topics that broaden my perspective.",
+];
 
-  export const skills = [
-	{
-	  name: "HTML",
-	  percent: 95,
-	  icon: <SiHtml5 className="text-orange-600" size={40} />,
-	},
-	{
-	  name: "CSS",
-	  percent: 90,
-	  icon: <SiCss3 className="text-blue-600" size={40} />,
-	},
-	{
-	  name: "JavaScript",
-	  percent: 70,
-	  icon: <SiJavascript className="text-yellow-400" size={40} />,
-	},
-	{
-	  name: "React",
-	  percent: 65,
-	  icon: <SiReact className="text-cyan-400" size={40} />,
-	},
-	{
-	  name: "Tailwind CSS",
-	  percent: 80,
-	  icon: <SiTailwindcss className="text-teal-400" size={40} />,
-	},
-	{
-	  name: "Bootstrap",
-	  percent: 85,
-	  icon: <SiBootstrap className="text-purple-700" size={40} />,
-	},
-	{
-	  name: "Node.js",
-	  percent: 30,
-	  icon: <SiNodedotjs className="text-green-600" size={40} />,
-	},
-	{
-	  name: "Express.js",
-	  percent: 25,
-	  icon: <SiExpress className="text-gray-800" size={40} />,
-	},
-	{
-	  name: "MongoDB",
-	  percent: 50,
-	  icon: <SiMongodb className="text-green-700" size={40} />,
-	},
-  ];
+export const skills = [
+  {
+    name: "HTML",
+    percent: 95,
+    icon: <SiHtml5 className="text-orange-600" size={40} />,
+  },
+  {
+    name: "CSS",
+    percent: 90,
+    icon: <SiCss3 className="text-blue-600" size={40} />,
+  },
+  {
+    name: "JavaScript",
+    percent: 70,
+    icon: <SiJavascript className="text-yellow-400" size={40} />,
+  },
+  {
+    name: "React",
+    percent: 65,
+    icon: <SiReact className="text-cyan-400" size={40} />,
+  },
+  {
+    name: "Tailwind CSS",
+    percent: 80,
+    icon: <SiTailwindcss className="text-teal-400" size={40} />,
+  },
+  {
+    name: "Bootstrap",
+    percent: 85,
+    icon: <SiBootstrap className="text-purple-700" size={40} />,
+  },
+  {
+    name: "Node.js",
+    percent: 30,
+    icon: <SiNodedotjs className="text-green-600" size={40} />,
+  },
+  {
+    name: "Express.js",
+    percent: 25,
+    icon: <SiExpress className="text-gray-800" size={40} />,
+  },
+  {
+    name: "MongoDB",
+    percent: 50,
+    icon: <SiMongodb className="text-green-700" size={40} />,
+  },
+];
